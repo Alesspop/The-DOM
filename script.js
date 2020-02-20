e2623661d6dd1535bb04488d952d54cfe88f9590
Alesspop <46914352+Alesspop@users.noreply.github.com>
2020-02-19 23:17:51 -0600

    switch function

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ var textNode = document.createTextNode (" This is the text node I created.");
 // append Child
 myDemo.appendChild(createElement);
 createElement.appendChild(textNode);
+
+var coupon= prompt ("Would you like a 20% off code for your first online order?");
+switch(coupon) {
+    case "yes":
+    alert ("BAKE20");
+    break;
+
+    case "yea":
+    alert ("BAKE20");
+    break;
+
+    case "yup":
+    alert ("BAKE20");
+    break;
+
+    case "no":
+    alert ("You sure? Come back if you change your mind!");
+    break;
+}
